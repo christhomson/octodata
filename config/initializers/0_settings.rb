8b41d5fe37978d80cd58dc75c581f29b47e2a25f
Chris Thomson <chris@cthomson.ca>
2014-02-27 21:01:31 -0500

    Access settings with indifferent access.

--- config/initializers/0_settings.rb
+++ config/initializers/0_settings.rb
@@ -1,7 +1,7 @@
 filename = File.join(Rails.root, "config/settings.yml")
 
 if File.exist?(filename)
-  Rails.configuration.settings = YAML.load(File.read(filename))[Rails.env]
+  Rails.configuration.settings = YAML.load(File.read(filename))[Rails.env].with_indifferent_access
 else
   Rails.logger.fatal "Could not find config/settings.yml."
   Rails.configuration.settings = {}
