846a759a7704916564488831a507a2a0f62f0c90
Chris Thomson <chris@cthomson.ca>
2014-03-02 15:57:21 -0500

    Add PullRequestReviewCommentEvents.

--- app/models/events/pull_request_review_comment_event.rb
+++ app/models/events/pull_request_review_comment_event.rb
@@ -1,2 +1,21 @@
 class PullRequestReviewCommentEvent < Event
+  belongs_to :comment
+  validates :comment, presence: true
+
+  def github_event=(github_event)
+    self.build_comment(comment_attributes(github_event)).save
+  end
+
+  def action_description
+    "#{user.username} commented on a pull request"
+  end
+
+  private
+  def comment_attributes
+    github_event.payload.comment.to_hash.tap do |comment|
+      comment.delete('user')
+      comment.delete('id')
+      comment[:remote_id] = github_event.payload.comment.id
+    end
+  end
 end
---
+++ test/models/pull_request_review_comment_event_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class PullRequestReviewCommentEventTest < ActiveSupport::TestCase
+  setup do
+    @event = events(:pull_request_review_comment)
+  end
+
+  test "should require a comment" do
+    @event.comment = nil
+
+    refute @event.valid?, "PullRequestReviewCommentEvent should require a comment, but it didn't."
+  end
+
+  test "should have an action description" do
+    assert @event.action_description.present?, "PullRequestReviewCommentEvent should have an action description, but it didn't."
+  end
+end
