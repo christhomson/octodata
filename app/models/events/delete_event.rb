9366f690157bf26798b2791ac1afcae85e2563bb
Chris Thomson <chris@cthomson.ca>
2014-03-02 16:38:20 -0500

    Fix incorrect method name in DeleteEvent.

--- app/models/events/delete_event.rb
+++ app/models/events/delete_event.rb
@@ -3,9 +3,9 @@ class DeleteEvent < Event
   validates :ref_type, presence: true, inclusion: { in: %w{branch tag} }
   validates :ref, presence: true
 
-  def github_client=(github_client)
+  def github_event=(github_event)
     [:ref_type, :ref].each do |attr|
-      self[attr] = github_client.payload[attr]
+      self[attr] = github_event.payload[attr]
     end
   end
 
