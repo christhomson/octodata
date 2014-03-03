a09438f0c1002ba4b82aca778d733a979361a0b4
Chris Thomson <chris@cthomson.ca>
2014-03-02 20:22:12 -0500

    Add IssuesEvents.

--- app/models/events/issues_event.rb
+++ app/models/events/issues_event.rb
@@ -1,2 +1,35 @@
 class IssuesEvent < Event
+  belongs_to :issue
+
+  validates :issue, presence: true
+  validates :action, presence: true
+  validates :repository, presence: true
+
+  def github_event=(github_event)
+    self.action = github_event.payload.action
+    self.build_issue(issue_attributes(github_event)).save!
+  end
+
+  def action_description
+    "#{user.username} #{action} issue ##{issue.number} on #{repository.display_name}"
+  end
+
+  private
+  def issue_attributes(github_event)
+    # TODO - duplication - move this into the Issue model.
+    github_event.payload.issue.to_hash.with_indifferent_access.tap do |issue|
+      issue[:remote_id] = issue[:id]
+      issue[:remote_created_at] = issue[:created_at]
+      issue[:url] = issue[:html_url]
+      issue[:creator] = issue[:user][:login]
+      issue[:pull_request] = !!issue[:pull_request]
+      issue[:repository_id] = self.repository.id
+
+      attrs_to_keep = %w{
+        url remote_id number title creator state assignee milestone
+        comments remote_created_at closed_at pull_request body repository_id 
+      }
+      issue.send(:slice!, *attrs_to_keep)
+    end
+  end
 end
---
+++ test/models/issues_event_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class IssuesEventTest < ActiveSupport::TestCase
+  setup do
+    @event = events(:issues)
+  end
+
+  test "should be valid" do
+    assert @event.valid?
+  end
+
+  test "should require a repository" do
+    @event.repository = nil
+
+    refute @event.valid?, "IssuesEvent should require a repository, but it didn't."
+  end
+
+  test "should require an issue" do
+    @event.issue = nil
+
+    refute @event.valid?, "IssuesEvent should require a repository, but it didn't."
+  end
+
+  test "should require an action" do
+    @event.action = nil
+
+    refute @event.valid?, "IssuesEvent should require an action, but it didn't."
+  end
+
+  test "should have an action description" do
+    assert @event.action_description.present?, "IssuesEvent should have an action description."
+  end
+end
