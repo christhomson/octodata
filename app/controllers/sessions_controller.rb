9aaefcfba78d56440e21875feefd44c1827f7d87
Chris Thomson <chris@cthomson.ca>
2014-03-03 22:29:35 -0500

    Force authentication everywhere.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :ensure_logged_in
+
   def create
     user = User.from_auth(auth_hash)
     session[:username] = user.username
