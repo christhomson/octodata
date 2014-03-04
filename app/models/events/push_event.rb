4a773feb44b05121db73726918e594784989b0b7
Chris Thomson <chris@cthomson.ca>
2014-03-03 19:51:51 -0500

    Properly pluralize commit in action_description.

--- app/models/events/push_event.rb
+++ app/models/events/push_event.rb
@@ -22,6 +22,6 @@ class PushEvent < Event
   end
 
   def action_description
-    "pushed #{size} commits to #{repository.display_name}"
+    "pushed #{pluralize(size, 'commit')} to #{repository.display_name}"
   end
 end
