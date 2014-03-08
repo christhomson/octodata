05ce6ccf84bc5bc5f3f5883b54d2f00ce0062bf8
Chris Thomson <chris@cthomson.ca>
2014-03-07 22:30:55 -0500

    Add has_many relations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,34 @@
 class User < ActiveRecord::Base
   has_many :imports
   has_many :events
+
+  has_many :commit_comment_events
+  has_many :create_events
+  has_many :delete_events
+  has_many :deployment_events
+  has_many :deployment_status_events
+  has_many :follow_events
+  has_many :fork_events
+  has_many :gollum_events
+  has_many :issue_comment_events
+  has_many :issues_events
+  has_many :member_events
+  has_many :public_events
+  has_many :pull_request_events
+  has_many :pull_request_review_comment_events
+  has_many :push_events
+  has_many :release_events
+  has_many :status_events
+  has_many :team_add_events
+  has_many :watch_events
+
   has_many :repositories, -> { uniq }, through: :events
+
+  has_many :commits, through: :push_events
+  has_many :pull_requests, through: :pull_request_events
+  has_many :issue_comments, through: :issue_comment_events, source: :comment
+  has_many :pull_request_review_comments, through: :pull_request_review_comment_events, source: :comment
+  has_many :commit_comments, through: :commit_comment_events, source: :comment
 
   validates :username, presence: true
   validates :auth_token, presence: true
