dfb6b179a956799c7ced24dcabcf85d8508f6236
Chris Thomson <chris@cthomson.ca>
2014-03-17 20:53:39 -0400

    Add repository links.

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -6,4 +6,8 @@ module ActivityHelper
   def render_event(event)
     render event.class.to_s.underscore, event: event
   end
+
+  def link_to_repository(repository)
+    link_to repository.display_name, "https://github.com/#{repository.display_name}"
+  end
 end
