4ca25de7b939ad25a526f030922ee2d209889ace
Chris Thomson <chris@cthomson.ca>
2014-04-02 22:08:37 -0400

    Remove unneeded action_description for PullRequestReviewCommentEvent.

--- app/models/events/pull_request_review_comment_event.rb
+++ app/models/events/pull_request_review_comment_event.rb
@@ -5,8 +5,4 @@ class PullRequestReviewCommentEvent < Event
   def github_event=(github_event)
     self.build_comment.assign_filtered_attributes(github_event)
   end
-
-  def action_description
-    "commented on a pull request on #{repository.display_name}"
-  end
 end
